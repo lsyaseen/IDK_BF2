9001cd51460625dd22d46ad2905e2600abcb44b9
Lama Yaseen <lsyaseens@gmail.com>
2021-11-11 10:20:55 +0300

    Added virtual env requirements and gunicorn conf files

--- idk/Scripts/activate.bat
+++ idk/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\lyase\Desktop\Randomizer\idk
+set VIRTUAL_ENV=C:\Users\lyase\OneDrive\03_Development\IDK_BF2\idk
 
 if not defined PROMPT set PROMPT=$P$G
 
